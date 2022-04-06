a5766dc519297987d6ce745a9ae2953631bd9e7e
Laszlo Jakab <6196882+laszlojakab@users.noreply.github.com>
2022-04-06 22:35:30 +0200

    feat: add spatial transformation matrices

--- custom_components/door_and_window/tests/transformers/test_coordinate_transformations.py
+++ custom_components/door_and_window/tests/transformers/test_coordinate_transformations.py
@@ -1,12 +1,11 @@
 """ Tests for coordinate transformations """
-# pylint: disable=missing-function-docstring
-
 import numpy as np
 
 from ...transformers.coordinate_transformations import CoordinateTransformations
 
 
 def test_convert_polar_coordinates_to_rectanglar():
+    """ Tests the `convert_polar_coordinates_to_rectanglar` method """
     transformations = CoordinateTransformations()
 
     assert np.isclose(
@@ -27,4 +26,80 @@ def test_convert_polar_coordinates_to_rectanglar():
     assert np.isclose(
         transformations.convert_polar_coordinates_to_rectanglar(90, 0),
         (0, 1, 0)
+    ).all()
+
+
+def test_rotation_matrix_x():
+    """ Tests if the rotation matrix around X axis is valid """
+
+    transformations = CoordinateTransformations()
+
+    assert np.isclose(
+        transformations.get_rotation_matrix_x(0),
+        [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ]
+    ).all()
+
+    assert np.isclose(
+        transformations.get_rotation_matrix_x(90),
+        [
+            [1, 0, 0, 0],
+            [0, 0, -1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1]
+        ]
+    ).all()
+
+def test_rotation_matrix_y():
+    """ Tests if the rotation matrix around Y axis is valid """
+
+    transformations = CoordinateTransformations()
+
+    assert np.isclose(
+        transformations.get_rotation_matrix_y(0),
+        [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ]
+    ).all()
+
+    assert np.isclose(
+        transformations.get_rotation_matrix_y(90),
+        [
+            [0, 0, 1, 0],
+            [0, 1, 0, 0],
+            [-1, 0, 0, 0],
+            [0, 0, 0, 1]
+        ]
+    ).all()
+
+def test_translation_matrix():
+    """ Tests if the translation matrix is valid """
+
+    transformations = CoordinateTransformations()
+
+    assert np.isclose(
+        transformations.get_translation_matrix(0, 0, 0),
+        [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ]
+    ).all()
+
+    assert np.isclose(
+        transformations.get_translation_matrix(1, 2, 3),
+        [
+            [1, 0, 0, 1],
+            [0, 1, 0, 2],
+            [0, 0, 1, 3],
+            [0, 0, 0, 1]
+        ]
     ).all()
--- custom_components/door_and_window/transformers/coordinate_transformations.py
+++ custom_components/door_and_window/transformers/coordinate_transformations.py
@@ -2,6 +2,11 @@
 import math
 from typing import Tuple
 
+import numpy as np
+
+# Rotation matrix based on:
+# https://www.brainvoyager.com/bv/doc/UsersGuide/CoordsAndTransforms/SpatialTransformationMatrices.html
+
 # pylint: disable=too-few-public-methods
 class CoordinateTransformations():
     """ Class for coordinate transformations. """
@@ -41,3 +46,70 @@ class CoordinateTransformations():
         (sin_elevation, cos_elevation) = self._get_sin_cos(90 - elevation)
 
         return (sin_elevation*cos_azimuth, sin_elevation*sin_azimuth, cos_elevation)
+
+
+    def get_rotation_matrix_x(self, angle: float) -> np.ndarray:
+        """
+        Gets the rotation matrix for rotating a vector on X axis in 3D
+        by the specified angle (in degrees).
+
+        Args:
+            angle:
+                The angle of rotation in degrees.
+
+        Returns:
+            The numpy array contains the rotation matrix.
+        """
+        (sin, cos) = self._get_sin_cos(angle)
+
+        return np.array([
+            [1, 0, 0, 0],
+            [0, cos, -sin, 0],
+            [0, sin, cos, 0],
+            [0, 0, 0, 1]
+        ])
+
+    def get_rotation_matrix_y(self, angle: float) -> np.ndarray:
+        """
+        Gets the rotation matrix for rotating a vector on Y axis in 3D
+        by the specified angle (in degrees).
+
+        Args:
+            angle:
+                The angle of rotation in degrees.
+
+        Returns:
+            The numpy array contains the rotation matrix.
+        """
+        (sin, cos) = self._get_sin_cos(angle)
+
+        return np.array([
+            [cos, 0., sin, 0.],
+            [0., 1., 0., 0.],
+            [-sin, 0., cos, 0.],
+            [0., 0., 0., 1.]
+        ])
+
+    # pylint: disable=invalid-name
+    @classmethod
+    def get_translation_matrix(cls, x: float, y: float, z: float) -> np.ndarray:
+        """
+        Gets the translation matrix for 3D translation
+
+        Args:
+            x:
+                The translation amount on X axis.
+            y:
+                The translation amount on Y axis.
+            z:
+                The translation amount on Z axis.
+
+        Returns:
+            The numpy array contains the translation matrix.
+        """
+        return np.array([
+            [1., 0., 0., x],
+            [0., 1., 0., y],
+            [0., 0., 1., z],
+            [0., 0., 0., 1.]
+        ])
